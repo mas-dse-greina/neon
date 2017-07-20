13fc4a506a042234974ce88f535f224472444c41
Tony <greina@eng.ucsd.edu>
2017-07-20 14:13:11 -0700

    processing for extracting luna16 patches

---
+++ dicom/process_LUNA16.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python LUNA16_extract_patches.py --subset=subset0
+python LUNA16_extract_patches.py --subset=subset1
+python LUNA16_extract_patches.py --subset=subset2
+python LUNA16_extract_patches.py --subset=subset3
+python LUNA16_extract_patches.py --subset=subset4
+python LUNA16_extract_patches.py --subset=subset5
+python LUNA16_extract_patches.py --subset=subset6
+python LUNA16_extract_patches.py --subset=subset7
+python LUNA16_extract_patches.py --subset=subset8
+python LUNA16_extract_patches.py --subset=subset9
