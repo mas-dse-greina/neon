75400178154dd32563ea6686001fcc3063b1dcb5
Tony Reina <greina@eng.ucsd.edu>
2017-09-08 18:45:04 -0700

    Script for processing

---
+++ luna16/process_LUNA16.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+# python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 0
+# python LUNA16_extract_patches_subset_HDF5.py --augment --subset 0
+# python LUNA16_extract_patches_subset_HDF5.py --subset 0
+
+# python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 1
+# python LUNA16_extract_patches_subset_HDF5.py --augment --subset 1
+# python LUNA16_extract_patches_subset_HDF5.py --subset 1
+
+# python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 2
+# python LUNA16_extract_patches_subset_HDF5.py --augment --subset 2
+# python LUNA16_extract_patches_subset_HDF5.py --subset 2
+
+python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 3
+python LUNA16_extract_patches_subset_HDF5.py --augment --subset 3
+python LUNA16_extract_patches_subset_HDF5.py --subset 3
+
+python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 4
+python LUNA16_extract_patches_subset_HDF5.py --augment --subset 4
+python LUNA16_extract_patches_subset_HDF5.py --subset 4
+
+python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 5
+python LUNA16_extract_patches_subset_HDF5.py --augment --subset 5
+python LUNA16_extract_patches_subset_HDF5.py --subset 5
+
+python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 6
+python LUNA16_extract_patches_subset_HDF5.py --augment --subset 6
+python LUNA16_extract_patches_subset_HDF5.py --subset 6
+
+python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 7
+python LUNA16_extract_patches_subset_HDF5.py --augment --subset 7
+python LUNA16_extract_patches_subset_HDF5.py --subset 7
+
+python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 8
+python LUNA16_extract_patches_subset_HDF5.py --augment --subset 8
+python LUNA16_extract_patches_subset_HDF5.py --subset 8
+
+# python LUNA16_extract_patches_subset_HDF5.py --exclude --augment --subset 9
+# python LUNA16_extract_patches_subset_HDF5.py --augment --subset 9
+# python LUNA16_extract_patches_subset_HDF5.py --subset 9
+
